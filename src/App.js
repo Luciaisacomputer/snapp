8710a8d23e10719136b4598da11d43223084005b
Luke Pettway <luke.pettway@thinkbrownstone.com>
2017-08-22 09:11:46 -0400

    Updated text

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Sprint Naming App</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a useful tool to help make it easier to name your sprint cycles using food, pop culture, and just about anything else!
         </p>
       </div>
     );
